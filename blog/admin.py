5d6bdcd47036696bfd2f4dcfcb9f7159e56d595d
StanislavVNikitin <stasvnikitin@gmail.com>
2023-02-07 17:23:56 +0300

    Init Blog app. Post, Category, Tag model

--- blog/admin.py
+++ blog/admin.py
@@ -1,58 +1,60 @@
+from ckeditor_uploader.widgets import CKEditorUploadingWidget
 from django import forms
 from django.contrib import admin
-from ckeditor_uploader.widgets import CKEditorUploadingWidget
 from django.utils.safestring import mark_safe
-
-# Register your models here.
 
 from .models import *
 
+# Register your models here.
+
+
 class PostAdminForm(forms.ModelForm):
     content = forms.CharField(widget=CKEditorUploadingWidget())
+
     class Meta:
         model = Post
-        fields = '__all__'
+        fields = "__all__"
+
 
 class PostAdmin(admin.ModelAdmin):
-    prepopulated_fields = {"slug": ('title',)}
+    prepopulated_fields = {"slug": ("title",)}
     form = PostAdminForm
     save_as = True
     save_on_top = True
-    list_display = ('id', 'title', 'slug', 'category', 'created_at', 'get_photo')
-    list_display_links = ('id', 'title')
-    search_fields = ('title',)
-    list_filter = ('category',)
-    readonly_fields = ('views','created_at','get_photo')
-    fields = ('title', 'slug', 'category', 'tags', 'content', 'photo', 'get_photo', 'views', 'created_at')
+    list_display = ("id", "title", "slug", "category", "created_at", "get_photo")
+    list_display_links = ("id", "title")
+    search_fields = ("title",)
+    list_filter = ("category",)
+    readonly_fields = ("views", "created_at", "get_photo")
+    fields = ("title", "slug", "category", "tags", "content", "photo", "get_photo", "views", "created_at")
 
-    def get_photo(self,obj):
+    def get_photo(self, obj):
         if obj.photo:
             return mark_safe(f'<img src="{obj.photo.url}" width="50">')
-        return '-'
+        return "-"
 
-    get_photo.short_description='Фото'
-
-
+    get_photo.short_description = "Фото"
 
 
 class CategoryAdmin(admin.ModelAdmin):
-    prepopulated_fields = {"slug": ('title',)}
+    prepopulated_fields = {"slug": ("title",)}
     save_as = True
     save_on_top = True
-    list_display = ('id', 'title', 'slug')
-    list_display_links = ('id', 'title')
-    search_fields = ('title',)
-    fields = ('title', 'slug')
+    list_display = ("id", "title", "slug")
+    list_display_links = ("id", "title")
+    search_fields = ("title",)
+    fields = ("title", "slug")
 
 
 class TagAdmin(admin.ModelAdmin):
-    prepopulated_fields = {"slug": ('title',)}
+    prepopulated_fields = {"slug": ("title",)}
     save_as = True
     save_on_top = True
-    list_display = ('id', 'title', 'slug')
-    list_display_links = ('id', 'title')
-    search_fields = ('title',)
-    fields = ('title', 'slug')
+    list_display = ("id", "title", "slug")
+    list_display_links = ("id", "title")
+    search_fields = ("title",)
+    fields = ("title", "slug")
+
 
 admin.site.register(Category, CategoryAdmin)
 admin.site.register(Tag, TagAdmin)
--- blog/models.py
+++ blog/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 
-'''
+"""
 Category
 ========
 title,slug
@@ -12,47 +12,50 @@ title,slug
 Post
 =========
 title, slug, author, content, created_at, photo, views,category, tags 
-'''
+"""
+
 
 class Category(models.Model):
-    title = models.CharField(max_length=255, verbose_name='Название')
-    slug = models.SlugField(max_length=255, verbose_name='Url категории', unique=True)
+    title = models.CharField(max_length=255, verbose_name="Название")
+    slug = models.SlugField(max_length=255, verbose_name="Url категории", unique=True)
+
     def __str__(self):
         return self.title
 
     class Meta:
-        verbose_name = 'Категория'
-        verbose_name_plural = 'Категории'
-        ordering = ['title']
+        verbose_name = "Категория"
+        verbose_name_plural = "Категории"
+        ordering = ["title"]
+
 
 class Tag(models.Model):
-    title = models.CharField(max_length=50, verbose_name='Название')
-    slug = models.SlugField(max_length=50, verbose_name='Url тэга', unique=True)
+    title = models.CharField(max_length=50, verbose_name="Название")
+    slug = models.SlugField(max_length=50, verbose_name="Url тэга", unique=True)
 
     def __str__(self):
         return self.title
 
     class Meta:
-        verbose_name = 'Тэг'
-        verbose_name_plural = 'Тэги'
-        ordering = ['title']
+        verbose_name = "Тэг"
+        verbose_name_plural = "Тэги"
+        ordering = ["title"]
 
 
 class Post(models.Model):
-        title = models.CharField(max_length=255, verbose_name='Название новости')
-        slug = models.SlugField(max_length=255, verbose_name='Url новости', unique=True)
-        author = models.CharField(max_length=100, verbose_name='Автор')
-        content = models.TextField(blank=True, verbose_name='Текст новости')
-        created_at = models.DateTimeField(auto_now_add=True, verbose_name='Опубликовано')
-        photo = models.ImageField(upload_to='photo/%Y/%m/%d/', blank=True, verbose_name='Картинка')
-        views = models.IntegerField(default=0, verbose_name='Количество просмотров')
-        category = models.ForeignKey('Category', on_delete=models.PROTECT, related_name='posts', verbose_name='Категория')
-        tags = models.ManyToManyField('Tag', blank=True, related_name='posts', verbose_name='Тэг')
+    title = models.CharField(max_length=255, verbose_name="Название новости")
+    slug = models.SlugField(max_length=255, verbose_name="Url новости", unique=True)
+    author = models.CharField(max_length=100, verbose_name="Автор")
+    content = models.TextField(blank=True, verbose_name="Текст новости")
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name="Опубликовано")
+    photo = models.ImageField(upload_to="photo/%Y/%m/%d/", blank=True, verbose_name="Картинка")
+    views = models.IntegerField(default=0, verbose_name="Количество просмотров")
+    category = models.ForeignKey("Category", on_delete=models.PROTECT, related_name="posts", verbose_name="Категория")
+    tags = models.ManyToManyField("Tag", blank=True, related_name="posts", verbose_name="Тэг")
 
-        def __str__(self):
-            return self.title
+    def __str__(self):
+        return self.title
 
-        class Meta:
-            verbose_name = 'Статья'
-            verbose_name_plural = 'Статьи'
-            ordering = ['-created_at']
+    class Meta:
+        verbose_name = "Статья"
+        verbose_name_plural = "Статьи"
+        ordering = ["-created_at"]
