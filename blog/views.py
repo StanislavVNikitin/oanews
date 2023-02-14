1365c1fa021e1c4367fc355d2b6b7ba45cb32086
StanislavVNikitin <stasvnikitin@gmail.com>
2023-02-14 19:53:29 +0300

    Update base templates

--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from django.views.generic import ListView
 
-from .models import Post
+from .models import Category, Post
 
 
 class Home(ListView):
@@ -13,6 +13,7 @@ class Home(ListView):
     def get_context_data(self, *, object_list=None, **kwargs):
         context = super().get_context_data(**kwargs)
         context["title"] = "Органические ацидурии"
+        context["categories"] = Category.objects.all()
         return context
 
 
