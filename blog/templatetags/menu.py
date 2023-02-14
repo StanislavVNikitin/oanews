ff463433dce010e9642873b097e5321080eed30e
StanislavVNikitin <stasvnikitin@gmail.com>
2023-02-14 13:26:03 +0300

    Update base templates

---
+++ blog/templatetags/menu.py
@@ -0,0 +1,10 @@
+from django import template
+from blog.models import Category
+
+register = template.Library()
+
+@register.inclusion_tag('blog/menu_tpl.html')
+def show_menu(menu_class='menu'):
+    categoies = Category.objects.all()
+    return {"categories": categoies, 'menu_class': menu_class}
+
