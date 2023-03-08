1b86c1880de678e494bb9ccea5dc4dd81e462475
StanislavVNikitin <stasvnikitin@gmail.com>
2023-03-08 15:41:57 +0300

    Update urls

--- config/urls.py
+++ config/urls.py
@@ -19,7 +19,7 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('akabinet/', admin.site.urls),
     path('ckeditor/', include('ckeditor_uploader.urls')),
     path('captcha/',include('captcha.urls')),
     path('blog/', include("blog.urls")),
