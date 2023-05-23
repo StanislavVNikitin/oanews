3b4b9c4df7b4eed0a8d7080a1a1af128da40377e
StanislavVNikitin <stasvnikitin@gmail.com>
2023-05-23 11:57:01 +0300

    bug fix spec_food_form

--- dietcalc/forms/spec_food_form.py
+++ dietcalc/forms/spec_food_form.py
@@ -5,7 +5,7 @@ from dietcalc.models import Food
 
 class SpecialFoodCalcForm(forms.Form):
     food = forms.ModelChoiceField(label="Специальное питание(аминокислотной смеси)",queryset=Food.objects.filter(deleted=False,special_food=True,is_published=True))
-    weight = forms.IntegerField(label="Вес пациента в килограмах")
-    norm_per_kg = forms.DecimalField(label="Норма грамм белка спец.смести(аминокислотной смеси) на килогорамм веса.")
+    weight = forms.IntegerField(label="Вес пациента в килограммах")
+    norm_per_kg = forms.DecimalField(label="Норма грамм белка спец.смести(аминокислотной смеси) на килограмм веса.")
 
 
