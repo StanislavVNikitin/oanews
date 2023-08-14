268cba7e24ad06042b56b7986936320af1606b03
stas <stasvnikitin@mail.ru>
2023-08-14 13:01:01 +0300

    Insert validation food forms

--- dietcalc/forms/my_food_form.py
+++ dietcalc/forms/my_food_form.py
@@ -1,12 +1,29 @@
 __all__ = ["CreateAndUpdateMyFoodForm"]
 
+import decimal
+from _decimal import Decimal
+
 from django import forms
 
 from dietcalc.models import Food
+from django.core.exceptions import ValidationError
 
 class CreateAndUpdateMyFoodForm(forms.ModelForm):
-    pass
+    def clean(self):
+        super().clean()
+        errors = {}
+        calories = self.cleaned_data['calories']
+        calculat_calories = float(self.cleaned_data['protein']) * 4 +  float(self.cleaned_data['carbohydrates']) * 4 + float(self.cleaned_data['fat']) * 9
+        diffcalories = float(calories)/calculat_calories
+        if (0.98 > diffcalories or diffcalories > 1.02):
+            print(diffcalories)
+            errors['calories'] = ValidationError(f'Расчетная сумма энергии от БЖУ отличается более 2% и составляет: {round((diffcalories*100)-100,2)}% от вносимой калорийности. Рассчетная из БЖУ:{int(calculat_calories)}Ккал, вводимая: {int(calories)}Ккал')
+        if errors:
+            raise ValidationError(errors)
+
 
     class Meta:
         model = Food
         fields = ['name', 'protein', 'carbohydrates', 'fat', 'calories']
+
+
