37a827b8baba90725c0e16a886fffd6b6e693e25
stas <stasvnikitin@mail.ru>
2023-12-26 15:17:22 +0300

    Save diet to PDF(Update PDF format)

--- dietcalc/views/diets/view.py
+++ dietcalc/views/diets/view.py
@@ -137,42 +137,45 @@ class PublicDietView(DetailView):
 def print_diet_pdf(request, pk):
     context = {}
     user_diet = get_object_or_404(UserDiet, id=pk)
-    context['userdiet']=user_diet
-    context['title'] = "Диета: " + user_diet.name
-    context_diets = Diet.objects.filter(user_diet_id=user_diet.id, food__deleted=False).annotate(
-        f_protein=F('food__protein') * F('count') / 100, f_fat=F('food__fat') * F('count') / 100,
-        f_carbohydrates=F('food__carbohydrates') * F('count') / 100,
-        f_calories=F('food__calories') * F('count') / 100)
-    if context_diets.count() > 0:
-        context['diets'] = context_diets
-        context['diets_aggregate'] = context_diets.aggregate(sum_protein=Sum('f_protein'),
-                                                             sum_protein_nature=Sum('f_protein', filter=Q(
-                                                                 food__special_food=False)),
-                                                             sum_protein_amino=Sum('f_protein', filter=Q(
-                                                                 food__special_food=True)), sum_fat=Sum('f_fat'),
-                                                             sum_carbohydrates=Sum('f_carbohydrates'),
-                                                             sum_calories=Sum('f_calories'))
-        diet_procent_and_sum_weight = {
-            "procent_protein": context['diets_aggregate']['sum_protein'] * 400 / context['diets_aggregate'][
-                'sum_calories'],
-            "procent_fat": context['diets_aggregate']['sum_fat'] * 910 / context['diets_aggregate']['sum_calories'],
-            "procent_carbohydrates": context['diets_aggregate']['sum_carbohydrates'] * 400 /
-                                     context['diets_aggregate']['sum_calories'],
-            "sum_protein_weight": Decimal(context['diets_aggregate']['sum_protein']) / Decimal(
-                context['userdiet'].weight),
-            "sum_protein_nature_weight": context['diets_aggregate']['sum_protein_nature'] / Decimal(
-                context['userdiet'].weight) if context['diets_aggregate']['sum_protein_nature'] else 0,
-            "sum_protein_amino_weight": context['diets_aggregate']['sum_protein_amino'] / Decimal(
-                context['userdiet'].weight) if context['diets_aggregate']['sum_protein_amino'] else 0,
-            "sum_fat_weight": Decimal(context['diets_aggregate']['sum_fat']) / Decimal(context['userdiet'].weight),
-            "sum_carbohydrates_weight": Decimal(context['diets_aggregate']['sum_carbohydrates']) / Decimal(context[
-                                                                                                               'userdiet'].weight),
-            "sum_calories_weight": context['diets_aggregate']['sum_calories'] / Decimal(context['userdiet'].weight)
-        }
-        context['diets_aggregate'].update(diet_procent_and_sum_weight)
-    html = render_to_string('dietcalc/diets/my_diet_pdf.html',
-                            {'userdiet': context})
-    response = HttpResponse(content_type='application/pdf')
-    response['Content-Disposition'] = f'filename=Userdiet_{user_diet.id}_{user_diet.slug[0:(len(user_diet.slug)-16)]}.pdf'
-    weasyprint.HTML(string=html).write_pdf(response, stylesheets=[weasyprint.CSS(settings.STATIC_ROOT / 'css/pdf.css')])
-    return response
+    if user_diet.user_diet.user == request.user:
+        context['userdiet']=user_diet
+        context['title'] = "Диета: " + user_diet.name
+        context_diets = Diet.objects.filter(user_diet_id=user_diet.id, food__deleted=False).annotate(
+            f_protein=F('food__protein') * F('count') / 100, f_fat=F('food__fat') * F('count') / 100,
+            f_carbohydrates=F('food__carbohydrates') * F('count') / 100,
+            f_calories=F('food__calories') * F('count') / 100)
+        if context_diets.count() > 0:
+            context['diets'] = context_diets
+            context['diets_aggregate'] = context_diets.aggregate(sum_protein=Sum('f_protein'),
+                                                                 sum_protein_nature=Sum('f_protein', filter=Q(
+                                                                     food__special_food=False)),
+                                                                 sum_protein_amino=Sum('f_protein', filter=Q(
+                                                                     food__special_food=True)), sum_fat=Sum('f_fat'),
+                                                                 sum_carbohydrates=Sum('f_carbohydrates'),
+                                                                 sum_calories=Sum('f_calories'))
+            diet_procent_and_sum_weight = {
+                "procent_protein": context['diets_aggregate']['sum_protein'] * 400 / context['diets_aggregate'][
+                    'sum_calories'],
+                "procent_fat": context['diets_aggregate']['sum_fat'] * 910 / context['diets_aggregate']['sum_calories'],
+                "procent_carbohydrates": context['diets_aggregate']['sum_carbohydrates'] * 400 /
+                                         context['diets_aggregate']['sum_calories'],
+                "sum_protein_weight": Decimal(context['diets_aggregate']['sum_protein']) / Decimal(
+                    context['userdiet'].weight),
+                "sum_protein_nature_weight": context['diets_aggregate']['sum_protein_nature'] / Decimal(
+                    context['userdiet'].weight) if context['diets_aggregate']['sum_protein_nature'] else 0,
+                "sum_protein_amino_weight": context['diets_aggregate']['sum_protein_amino'] / Decimal(
+                    context['userdiet'].weight) if context['diets_aggregate']['sum_protein_amino'] else 0,
+                "sum_fat_weight": Decimal(context['diets_aggregate']['sum_fat']) / Decimal(context['userdiet'].weight),
+                "sum_carbohydrates_weight": Decimal(context['diets_aggregate']['sum_carbohydrates']) / Decimal(context[
+                                                                                                                   'userdiet'].weight),
+                "sum_calories_weight": context['diets_aggregate']['sum_calories'] / Decimal(context['userdiet'].weight)
+            }
+            context['diets_aggregate'].update(diet_procent_and_sum_weight)
+        html = render_to_string('dietcalc/diets/my_diet_pdf.html',
+                                {'userdiet': context})
+        response = HttpResponse(content_type='application/pdf')
+        response['Content-Disposition'] = f'filename=Userdiet_{user_diet.id}_{user_diet.slug[0:(len(user_diet.slug)-16)]}.pdf'
+        weasyprint.HTML(string=html).write_pdf(response, stylesheets=[weasyprint.CSS(settings.STATIC_ROOT / 'css/pdf.css')])
+        return response
+    else:
+        raise Http404()
